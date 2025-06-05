de302a38183c5ed2f8d5c4aba863a688487bcae5
Sandeep Reddy Bhumireddy <157559130+sandeepreddy2462@users.noreply.github.com>
2025-06-05 12:36:59 +0530

    Update search.js

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { WEATHER_API_URL, WEATHER_API_KEY } from "../../api";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "src\api.js";
 import "./search.css";
 
 const Search = ({ onSearchChange }) => {
