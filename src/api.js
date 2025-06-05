ae40030343e86bfdd276c33008e399d5b48367f9
Sandeep Reddy Bhumireddy <157559130+sandeepreddy2462@users.noreply.github.com>
2025-06-05 11:44:27 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
-export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-export const WEATHER_API_KEY = "f9028fd931477db1ec5e5be379235c31";
+export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather?q=";
